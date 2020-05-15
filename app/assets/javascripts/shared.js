0eef583bff453d16bb3c5564a2e2d80a4b630993
vridhi-sahu <vridhi.sahu.uemk.cse.15@gmail.com>
2020-05-15 23:13:10 +0500

    Update reservation calander functionality

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -15,44 +15,7 @@ $(document).ready(function() {
 
       selectable: true,
       selectHelper: true,
-      selectLongPressDelay:0.0001,
-
-      eventResize: function (event, delta, revertFunc) {
-        var start_date = event.start.format()
-        var end_date = event.end.format()
-        var title = event.title
-        $.ajax({
-           url: "/events/"+ event.id,
-           type: "put",
-           data: {
-            start_date: start_date, end_date: end_date, title: title
-           }
-        });
-      },
-
-      select: function (start_date, end_date, allDay) {
-        var start_date = start_date.format()
-        var end_date = end_date.format()
-        $.ajax({
-           url: "/events/new",
-           type: "get",
-           data: {
-            start_date: start_date, end_date: end_date
-           }
-        });
-      },
-
-      eventClick: function(calEvent, jsEvent, view) {
-        var ev_id= calEvent.id
-        var event_title= calEvent.title
-        $.ajax({
-           url: "/events/"+ev_id,
-           type: "get",
-           data: {
-            id: ev_id, title: event_title
-           }
-        });
-      }
+      selectLongPressDelay:0.0001
     });
   }
 
